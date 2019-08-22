1d554ecccca31f4ce0ffb78dc448ad8f54b504ea
fherneftali <fhernftali.fj@gmail.com>
2019-08-21 23:35:16 -0600

    Interface

---
+++ interface.js
@@ -0,0 +1,7 @@
+function area(shape) {
+    var area = shape.width * shape.height;
+    return "Soy un " + shape.name + " con un area de " + area + " cm²";
+}
+console.log(area({ name: "rectangulo", width: 30, height: 15 }));
+console.log(area({ name: "cuadrado", width: 30, height: 30, color: "blue" }));
+console.log(area({ width: 30, height: 15 }));
---
+++ interface.ts
@@ -0,0 +1,14 @@
+interface Shape{
+    name: string;
+    width: number;
+    height: number;
+    color?: string;
+}
+
+function area(shape: Shape){
+    var area = shape.width * shape.height;
+    return "Soy un " + shape.name + " con un area de " + area + " cm²";
+}
+ console.log( area( {name: "rectangulo", width: 30, height: 15}));
+ console.log( area( {name: "cuadrado", width: 30, height: 30, color: "blue"}));
+ console.log( area( {width: 30, height: 15}));
