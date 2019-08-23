a0f9e5e98c58ddf8a1dbe3b2284249c7bb90dcce
fherneftali <fhernftali.fj@gmail.com>
2019-08-23 17:55:23 -0600

    Propiedades de la interfaz opcionales

--- interfazLake.ts
+++ interfazLake.ts
@@ -4,7 +4,8 @@ interface Lakes{
     length: number,
     depth: number,
     isFreshwater: boolean,
-    countries: string[]
+    countries: string[],
+    frozen?: string[]
 }
 
 let firstLake: Lakes ={
@@ -14,4 +15,23 @@ let firstLake: Lakes ={
     area: 371000,
     isFreshwater: false,
     countries: ['Kazakhstan', 'Russia', 'Turkmenistan', 'Azerbaijan', 'Iran']
+}
+
+let secondLake: Lakes ={
+    name: 'Superior',
+    depth: 406.3,
+    length: 616,
+    area: 82100,
+    isFreshwater: true,
+    countries: ['Canada', 'United States']
+}
+
+let thirdLake: Lakes ={
+    name: 'Baikal',
+    depth: 1637,
+    length: 636,
+    area: 31500,
+    isFreshwater: true,
+    countries: ['Russia'],
+    frozen: ['January', 'February', 'March', 'April', 'May']
 }
