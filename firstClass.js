12a17396b008e9b66e50f8f4353da4d181919d12
fherneftali <fhernftali.fj@gmail.com>
2019-08-23 18:37:29 -0600

    Primera Clase

---
+++ firstClass.js
@@ -0,0 +1,11 @@
+var Person = /** @class */ (function () {
+    function Person(theName) {
+        this.name = theName;
+    }
+    Person.prototype.introduceSelf = function () {
+        console.log("Hola, soy " + this.name + "!");
+    };
+    return Person;
+}());
+var personA = new Person("Fher");
+personA.introduceSelf();
---
+++ firstClass.ts
@@ -0,0 +1,12 @@
+class Person{
+    name:string;
+    constructor(theName:string){
+        this.name= theName;
+    }
+    introduceSelf(){
+        console.log("Hola, soy " + this.name + "!");
+    }
+}
+
+let personA = new Person("Fher");
+personA.introduceSelf();
