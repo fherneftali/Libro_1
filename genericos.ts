e25b7f5657d0a7fabbed536330817e104fee3d1a
fherneftali <fhernftali.fj@gmail.com>
2019-08-23 19:56:50 -0600

    cadenas

--- genericos.ts
+++ genericos.ts
@@ -18,3 +18,12 @@ let randomPosition: number= randomElem(positions);
 
 let colors: string[] = ['violet', 'indigo', 'blue', 'green'];
 let randomColor: string = randomElem(colors);
+
+function removeChar(theString: string, theChar: string): string{
+    let theRegex = new RegExp(theChar, "gi");
+    return theString.replace(theRegex, '');
+}
+function removeIt<T>(theInput: T, theIt: string): T{
+    let theRegex = new RegExp(theIt, "gi");
+    return theInput.replace(theRegex, '');
+}
