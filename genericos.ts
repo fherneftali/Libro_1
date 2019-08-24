6daf30911fee47fe40bf749b53df055da2a8d25a
fherneftali <fhernftali.fj@gmail.com>
2019-08-23 19:41:24 -0600

    Genericos int y string

---
+++ genericos.ts
@@ -0,0 +1,16 @@
+function randomIntElem(theArray: number[]): number {
+    let randomIndex = Math.floor(Math.random()*theArray.length);
+    return theArray[randomIndex];
+}
+
+let positions: number[] = [103, 458, 472, 458];
+let randomPosition: number= randomIntElem(positions);
+
+function randomStrElem(theArray: string[]): string{
+    let randomIndex= Math.floor(Math.random()*theArray.length);
+    return theArray[randomIndex];
+}
+
+let colors: string[] = ['violet', 'indigo', 'blue', 'green'];
+let randomColor:string = randomStrElem(colors);
+
