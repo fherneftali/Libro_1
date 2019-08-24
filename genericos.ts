f434e9b19a9d084cc2f25eed2a1f84bfc810073d
fherneftali <fhernftali.fj@gmail.com>
2019-08-23 19:46:27 -0600

    funcion generica

--- genericos.ts
+++ genericos.ts
@@ -8,7 +8,7 @@ function randomStrElem(theArray: string[]): string{
     return theArray[randomIndex];
 }
 
-function randomElem(theArray: any[]): any{
+function randomElem<T>(theArray: T[]): T{
     let randomIndex = Math.floor(Math.random()*theArray.length);
     return theArray[randomIndex];
 }
@@ -17,4 +17,4 @@ let positions: number[] = [103, 458, 472, 458];
 let randomPosition: number= randomElem(positions);
 
 let colors: string[] = ['violet', 'indigo', 'blue', 'green'];
-let randomColor:string = randomElem(colors);
+let randomColor: string = randomElem(colors);
