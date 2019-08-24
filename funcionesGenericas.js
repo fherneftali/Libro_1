5f872d00d301182a1380c18936d4349b2dc659cc
fherneftali <fhernftali.fj@gmail.com>
2019-08-23 20:14:36 -0600

    Funciones Genericas

---
+++ funcionesGenericas.js
@@ -0,0 +1,8 @@
+function printName(theInput) {
+    console.log("Mi nombre es " + theInput.name);
+}
+var serena = {
+    name: 'Serena Williams',
+    profession: 'Tennis Player'
+};
+printName(serena);
